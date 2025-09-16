3206f2f76f60079c659a09fbece60156ce7e1f26
abubakarsial1234 <abubakarsial@Sial-pc.localdomain>
2025-09-16 11:29:51 +0500

    comments removed

--- modules/ecs/main.tf
+++ modules/ecs/main.tf
@@ -1,6 +1,5 @@
-# --- Naya Security Group aur uske Rules (agar user chahe) ---
+
 resource "alicloud_security_group" "sg" {
-  # Yeh resource sirf tab banega jab 'create_sg' variable true ho
   count  = var.create_sg ? 1 : 0
   
   name   = var.sg_name
@@ -8,7 +7,6 @@ resource "alicloud_security_group" "sg" {
 }
 
 resource "alicloud_security_group_rule" "ingress" {
-  # Yeh rules sirf tab baneinge jab naya SG ban raha ho
   for_each = var.create_sg ? { for rule in var.sg_rules : rule.port => rule } : {}
 
   type              = "ingress"
@@ -22,14 +20,13 @@ resource "alicloud_security_group_rule" "ingress" {
 }
 
 
-# --- ECS Instance Resource ---
+
 resource "alicloud_instance" "instance" {
   instance_type   = var.instance_type
   image_id        = var.image_id
   instance_name   = var.instance_name
   vswitch_id      = var.vswitch_id
   
-  # Shart (condition): Agar naya SG ban raha hai to uski ID istemal karo, warna jo IDs di gayi hain woh istemal karo
   security_groups = var.create_sg ? [alicloud_security_group.sg[0].id] : var.security_group_ids
 
   system_disk_category = "cloud_essd"
